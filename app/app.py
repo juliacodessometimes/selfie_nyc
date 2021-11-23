ca70686321e48026ef000f4780d2c2e7e05da68d
Julia <47335810+juliacodessometimes@users.noreply.github.com>
2021-11-23 17:29:29 -0500

    experiments with requests

--- app/app.py
+++ app/app.py
@@ -1,25 +1,48 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, send_file
 import geojson
+import urllib.request
 
 app = Flask(__name__)
 
+# importing geojson data
 with open('data.geojson') as g:
     data = geojson.load(g)
 
-@app.route('/')
+test_url = data['features'][20]['properties']['url']
+r = urllib.request.urlopen(test_url)
+with open("selfie_pause.jpg", "wb") as f:
+    f.write(r.read())
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
+
+@app.route('/image', methods=['GET', 'POST'])
+def image_grab():
+    if request.method == 'POST':
+        test = request.data
+        
+        print(test.decode('utf-8'))
+        
+        r = urllib.request.urlopen(test.decode('utf-8'))
+        with open("selfie_pause.jpg", "wb") as f:
+            f.write(r.read())
+            
+        return render_template("issue.html")
+    return 'nothing here lol'
+    
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# api endpoint
+# geojson endpoint
 @app.route('/data')
 def geodata():
     return jsonify(data)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(
         debug = True,
         port = 3000
