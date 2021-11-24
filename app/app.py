53a501e9824558c1d1911ba09523e17b7268eef2
Julia <47335810+juliacodessometimes@users.noreply.github.com>
2021-11-24 10:00:00 -0500

    backend now serves local images

--- app/app.py
+++ app/app.py
@@ -2,16 +2,13 @@ from flask import Flask, render_template, jsonify, request, send_file
 import geojson
 import urllib.request
 
+
 app = Flask(__name__)
 
 # importing geojson data
 with open('data.geojson') as g:
     data = geojson.load(g)
 
-test_url = data['features'][20]['properties']['url']
-r = urllib.request.urlopen(test_url)
-with open("selfie_pause.jpg", "wb") as f:
-    f.write(r.read())
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
@@ -20,18 +17,14 @@ def index():
 @app.route('/image', methods=['GET', 'POST'])
 def image_grab():
     if request.method == 'POST':
-        test = request.data
-        
-        print(test.decode('utf-8'))
-        
-        r = urllib.request.urlopen(test.decode('utf-8'))
-        with open("selfie_pause.jpg", "wb") as f:
+        image_url = request.data.decode('utf-8')
+                
+        r = urllib.request.urlopen(image_url)
+        with open('static/selfie_pause.jpg', "wb") as f:
             f.write(r.read())
             
-        return render_template("issue.html")
-    return 'nothing here lol'
+    return jsonify('../static/selfie_pause.jpg')
     
-
 
 @app.route('/about')
 def about():
