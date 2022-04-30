5a13546ee6b81722c5b8e62eb5fc076ee02f15e4
Julia Bloom <47335810+juliacodessometimes@users.noreply.github.com>
2022-04-30 11:35:44 -0400

    style test

--- docs/script/app.js
+++ docs/script/app.js
@@ -49,7 +49,7 @@ map.on('load', () => {
             'type': 'symbol',
             'source': 'source_id',
             'layout': {
-                'icon-image': 'icon',
+                'icon-image': 'icon_neon',
                 'icon-allow-overlap': true,
                 'icon-size': [
                     'interpolate', ['linear'], ['zoom'], 
